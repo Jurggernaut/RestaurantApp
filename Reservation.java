4051ed3b30f1502597e656eca4d110416c9d99e0
Bohemia44 <bohemia44@gmail.com>
2016-11-01 11:54:55 +0800

    Added checkDays for printing order batch

--- Reservation.java
+++ Reservation.java
@@ -1,48 +1,73 @@
 package Restaurant;
-import java.text.*; //Ignore warning, import is necessary
 import java.util.*;
+import java.text.*;
 
-public class Reservation {
+public class Reservation implements java.io.Serializable{
 	int ID;
 	int tableID;
-	int noOfPeople;
 	Calendar date;
+	
+	Reservation(){
+		ID = 0;
+		tableID = 0;
+		date = new GregorianCalendar(0,0,0,0,00);
+	}
 
 	
-	Reservation(int id, int tid, int ppl, Calendar d){
+	Reservation(int id, int tid, Calendar d){
 		ID = id;
 		tableID = tid;
-		noOfPeople = ppl;
 		date = d;
+}
+	public void clearReservation(){
+		ID = 0;
+		tableID = 0;
+		date = new GregorianCalendar(0, 0, 0, 0 ,00);
 	}
 	
-	public boolean checkTime(Calendar res) {
+	public void printReservation(){
+		System.out.println("ID = " + ID);
+		System.out.println("TableID = " + tableID);
+		Date printout = date.getTime();
+		SimpleDateFormat format1 = new SimpleDateFormat("y-M-d 'at' h:m");
+		String date1 = format1.format(printout); 
+	    System.out.println("Format 1:   " + date1);
+	}
+	
+
+	
+	public boolean checkTime() {
 		Calendar now = Calendar.getInstance();
-		res.add(Calendar.MINUTE, 30);
+		date.add(Calendar.MINUTE, 30);
 		now.add(Calendar.MONTH, 1);
-		res.add(Calendar.MONTH, 2);
-		if (res.compareTo(now) >= 0) {
+		date.add(Calendar.MONTH, 2);
+		if (date.compareTo(now) >= 0) {
 			//Current time is before or exactly at the arrival time + 30 minutes.
 			return true;
 		}
 		else {
 			// party is more than 30 minutes late, cancel reservation
 			return false;
-		}
+}
+	
 		
 	}
 	
-	public boolean checkTable(Calendar res) {
+	public int getTableID(){
+		return tableID;
+	}
+	
+	public boolean checkTable() {
         Calendar now = Calendar.getInstance();
         now.add(Calendar.MONTH, 1);
-        res.add(Calendar.MONTH, 2);
-        res.add(Calendar.HOUR, -2);
+        date.add(Calendar.MONTH, 2);
+        date.add(Calendar.HOUR, -2);
 
-        if (res.compareTo(now) >= 0) {
+        if (date.compareTo(now) >= 0) {
             return true;
         }
-        res.add(Calendar.MINUTE, 150);
-        if (res.compareTo(now) >= 0){
+        date.add(Calendar.MINUTE, 150);
+        if (date.compareTo(now) >= 0){
             return true;
         }
         else {
@@ -50,5 +75,17 @@ public class Reservation {
         }
 
     }
+
+	public boolean checkDays(int days, Calendar object) {
+		Calendar now = Calendar.getInstance();
+		now.add(Calendar.DAY_OF_MONTH, -days);
+		if (object.compareTo(now) >= 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 
 }
